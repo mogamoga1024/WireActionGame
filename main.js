e90cc13f9c88e6dbbd6cb26335841eb82155a4ba
mogamoga <mogamoga.g1thu6@gmail.com>
2024-08-03 15:44:48 +0900

    早く召し上がってねー

---
+++ main.js
@@ -0,0 +1,48 @@
+
+let isPressedLeft = false;
+let isPressedRight = false;
+let isPressedSpace = false;
+
+addEventListener("keydown", e => {
+    switch (e.key) {
+        case "ArrowLeft":  isPressedLeft = true;  break;
+        case "ArrowRight": isPressedRight = true; break;
+        case " ": isPressedSpace = true; break;
+    }
+});
+
+addEventListener("keyup", e => {
+    switch (e.key) {
+        case "ArrowLeft":  isPressedLeft = false;  break;
+        case "ArrowRight": isPressedRight = false; break;
+        case " ": isPressedSpace = false; break;
+    }
+});
+
+setInterval(() => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    update();
+}, 1000 / 60);
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 500;
+
+const player = new Player(100, 400);
+
+function update() {
+    if (isPressedLeft) {
+        player.run("left");
+    }
+    else if (isPressedRight) {
+        player.run("right");
+    }
+    else if (isPressedSpace) {
+        player.jump();
+    }
+
+    player.draw(context);
+}
+
---
+++ player.js
@@ -0,0 +1,28 @@
+
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.r = 20;
+    }
+
+    draw(context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+        context.fillStyle = "blue";
+        context.fill();
+    }
+
+    run(direction) {
+        if (direction === "left") {
+            this.x -= 10;
+        }
+        else if (direction === "right") {
+            this.x += 10;
+        }
+    }
+
+    jump() {
+        // todo
+    }
+}
