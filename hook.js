c5cf8e3ee2e2ecec89996fc5b64f11fed0e09b76
mogamoga <mogamoga.g1thu6@gmail.com>
2024-08-06 04:06:31 +0900

    フックも一緒に動かしたほうがいいな

--- hook.js
+++ hook.js
@@ -1,6 +1,6 @@
 
 class Hook {
-    #baseX = 0; #baseY = 0;
+    // #baseX = 0; #baseY = 0;
     #x = 0;
     get x() { return this.#x }
     #y = 0;
@@ -11,8 +11,8 @@ class Hook {
     get height() { return this.#height }
     get centerX() { return this.#x + this.#width / 2; }
     get centerY() { return this.#y + this.#height / 2; }
-    get baseCenterX() { return this.#baseX + this.#width / 2; }
-    get baseCenterY() { return this.#baseY + this.#height / 2; }
+    // get baseCenterX() { return this.#baseX + this.#width / 2; }
+    // get baseCenterY() { return this.#baseY + this.#height / 2; }
 
     #player = null;
     #v = 10;
@@ -23,8 +23,10 @@ class Hook {
     
     constructor(player, radian) {
         this.#player = player;
-        this.#baseX = this.#x = player.x + player.width / 2 - this.width / 2;
-        this.#baseY = this.#y = player.y + player.height / 2 - this.height / 2;
+        this.#x = player.x + player.width / 2 - this.width / 2;
+        this.#y = player.y + player.height / 2 - this.height / 2;
+        // this.#baseX = this.#x
+        // this.#baseY = this.#y
         this.#vx = this.#v * Math.cos(radian);
         this.#vy = -1 * this.#v * Math.sin(radian);
     }
@@ -42,14 +44,16 @@ class Hook {
             const vecX = this.centerX - this.#player.centerX;
             const vecY = this.centerY - this.#player.centerY;
             const radian = Math.atan2(vecY, vecX);
-            this.#vx = -1 * this.#v * Math.cos(radian);
-            this.#vy = -1 * this.#v * Math.sin(radian);
+            this.#vx = -1 * this.#v * 1.5 * Math.cos(radian);
+            this.#vy = -1 * this.#v * 1.5 * Math.sin(radian);
         }
         this.#x += this.#vx;
         this.#y += this.#vy;
         if (!this.#isShrinking) {
-            const diffX = this.centerX - this.baseCenterX;
-            const diffY = this.centerY - this.baseCenterY;
+            // const diffX = this.centerX - this.baseCenterX;
+            // const diffY = this.centerY - this.baseCenterY;
+            const diffX = this.centerX - this.#player.centerX;
+            const diffY = this.centerY - this.#player.centerY;
             if (Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2)) >= this.#maxWireLength) {
                 this.#isShrinking = true;
             }
