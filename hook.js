434cb828e325473a71c87f50967a5d42a4175165
mogamoga <mogamoga.g1thu6@gmail.com>
2024-08-06 10:51:56 +0900

    何かが目覚めそう

--- hook.js
+++ hook.js
@@ -1,6 +1,5 @@
 
 class Hook {
-    #baseX = 0; #baseY = 0;
     #relativeX = 0; #relativeY = 0;
     #x = 0;
     get x() { return this.#x }
@@ -12,8 +11,6 @@ class Hook {
     get height() { return this.#height }
     get centerX() { return this.#x + this.#width / 2; }
     get centerY() { return this.#y + this.#height / 2; }
-    get baseCenterX() { return this.#baseX + this.#width / 2; }
-    get baseCenterY() { return this.#baseY + this.#height / 2; }
 
     #player = null;
     #v = 10;
@@ -26,8 +23,6 @@ class Hook {
         this.#player = player;
         this.#x = player.x + player.width / 2 - this.width / 2;
         this.#y = player.y + player.height / 2 - this.height / 2;
-        this.#baseX = this.#x
-        this.#baseY = this.#y
         this.#vx = this.#v * Math.cos(radian);
         this.#vy = -1 * this.#v * Math.sin(radian);
     }
@@ -48,21 +43,20 @@ class Hook {
             this.#vx = -1 * this.#v * 1.5 * Math.cos(radian);
             this.#vy = -1 * this.#v * 1.5 * Math.sin(radian);
         }
-        this.#x += this.#vx;
-        this.#y += this.#vy;
+        this.#relativeX += this.#vx;
+        this.#relativeY += this.#vy;
+        this.#x = this.#player.centerX + this.#relativeX - this.#width / 2;
+        this.#y = this.#player.centerY + this.#relativeY - this.#height / 2;
+        const diffX = this.centerX - this.#player.centerX;
+        const diffY = this.centerY - this.#player.centerY;
+        const wireLength = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
         if (!this.#isShrinking) {
-            // const diffX = this.centerX - this.baseCenterX;
-            // const diffY = this.centerY - this.baseCenterY;
-            const diffX = this.centerX - this.#player.centerX;
-            const diffY = this.centerY - this.#player.centerY;
-            if (Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2)) >= this.#maxWireLength) {
+            if (wireLength >= this.#maxWireLength) {
                 this.#isShrinking = true;
             }
         }
         else {
-            const diffX = this.centerX - this.#player.centerX;
-            const diffY = this.centerY - this.#player.centerY;
-            if (Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2)) <= this.#v * 1.1) {
+            if (wireLength <= this.#v * 1.1) {
                 return true;
             }
         }
