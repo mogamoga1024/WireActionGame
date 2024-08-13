eb3a246c327627238c5690d2365ab1a19ba803a5
mogamoga <mogamoga.g1thu6@gmail.com>
2024-08-14 00:28:31 +0900

    たーのしー

--- main.js
+++ main.js
@@ -52,7 +52,10 @@ const player = new Player(100, 360);
 const staticObj1 = new StaticObject(0, 400, 1600, 100);
 const staticObj2 = new StaticObject(0, 0, 50, 500);
 const staticObj3 = new StaticObject(1600 - 50, 0, 50, 500);
-const staticObjList = [staticObj1, staticObj2, staticObj3];
+const staticObj4 = new StaticObject(300, 0, 1000, 50);
+const staticObj5 = new StaticObject(200, 320, 100, 80);
+const staticObj6 = new StaticObject(1600 - 200 - 100, 320, 100, 80);
+const staticObjList = [staticObj1, staticObj2, staticObj3, staticObj4, staticObj5, staticObj6];
 
 const viewport = new Viewport(0, 0, canvas.width, canvas.height, globalWidth, globalHeight, player);
 
