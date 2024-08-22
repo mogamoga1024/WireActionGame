64a47fe9c0e4c029be6f5922a9f140b1b9033dbd
mogamoga <mogamoga.g1thu6@gmail.com>
2024-08-22 20:37:32 +0900

    とりまこみっと

--- map_dev/main.js
+++ map_dev/main.js
@@ -15,6 +15,7 @@ const map = [
     [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
 ];
 
+const NONE = 0;
 const PLAYER = 1;
 const BLOCK = 2;
 const TRAMPOLINE = 3;
@@ -24,11 +25,14 @@ const unitSideLength = 30;
 
 const worldHeight = unitSideLength * map.length;
 
-const codeList = [];
+const blockList = [];
 let playerCode = "";
 
-for (let row = 0; row < map.length; row++) {
-    for (let col = 0; col < map[0].length; col++) {
+const rowMax = map.length;
+const colMax = map[0].length;
+
+for (let row = 0; row < rowMax; row++) {
+    for (let col = 0; col < colMax; col++) {
         const x = unitSideLength * col;
         const y = unitSideLength * row;
 
@@ -36,13 +40,45 @@ for (let row = 0; row < map.length; row++) {
 
         if (type === PLAYER) {
             playerCode = `player = new Player(${x}, ${y});`;
+            continue;
         }
+        else if (type === NONE) {
+            continue;
+        }
+
+        let width = unitSideLength;
+        let nextCol = col + 1;
+        while (nextCol < colMax) {
+            if (map[row][nextCol] !== type) {
+                break;
+            }
+            map[row][nextCol] = NONE;
+            width += unitSideLength;
+            nextCol++;
+        }
+
+        let height = unitSideLength;
+        let nextRow = row + 1;
+        while (nextRow < rowMax) {
+            for (let c = col; c < nextCol; c++) {
+                if (map[nextRow][c] !== type) {
+                    break;
+                }
+            }
+            for (let c = col; c < nextCol; c++) {
+                map[nextRow][c] = NONE;
+            }
+            height += unitSideLength;
+            nextRow++;
+        }
+
+        blockList.push(`bp(new Block(${x}, ${y}, ${width}, ${height}));`);
     }
 }
 
 let result = `worldHeight = ${worldHeight};\nconst h = worldHeight;\n`;
 
-for (const code of codeList) {
+for (const code of blockList) {
     result += code + "\n";
 }
 
