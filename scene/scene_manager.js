b73a4436d87ceef4146c1370957daeeda850ecbe
mogamoga <mogamoga.g1thu6@gmail.com>
2024-09-01 05:21:19 +0900

    多分こうしないとremoveできんと思う

--- scene/scene_manager.js
+++ scene/scene_manager.js
@@ -1,10 +1,17 @@
 
 class SceneManager {
     static #scene = null;
+    static #onKeyDown = null;
+    static #onKeyUp = null;
 
     static start(scene) {
         if (scene === null) {
             throw new Error("Sceneが未指定");
+        }
+
+        if (this.#scene !== null) {
+            window.removeEventListener("keydown", this.#onKeyDown);
+            window.removeEventListener("keyup", this.#onKeyUp);
         }
 
         const onKeyDown = e => {
@@ -13,11 +20,9 @@ class SceneManager {
         const onKeyUp = e => {
             this.#scene.onKeyUp(e)
         };
-
-        if (this.#scene !== null) {
-            window.removeEventListener("keydown", onKeyDown);
-            window.removeEventListener("keyup", onKeyUp);
-        }
+        
+        this.#onKeyDown = onkeydown;
+        this.#onKeyUp = onKeyUp;
 
         this.#scene = scene;
         this.#scene.onStart();
