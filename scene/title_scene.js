d5a8cff828fbac0445e1bc17904633182dd758a0
mogamoga <mogamoga.g1thu6@gmail.com>
2024-09-01 23:19:06 +0900

    title

--- scene/title_scene.js
+++ scene/title_scene.js
@@ -4,8 +4,25 @@ class TitleScene extends Scene {
         const canvas = document.querySelector("canvas");
         const context = canvas.getContext("2d");
 
-        context.font = "48px serif";
-        context.fillText("Hello world", 10, 50);
+        const titleText = "苦行系ワイヤーアクション";
+        context.font = "48px sans-serif";
+        const titleTextWidth = this.#textWidth(context, titleText);
+        context.fillText(titleText, (canvas.width - titleTextWidth) / 2, 150);
+
+        const saisyoText = "最初から";
+        context.font = "32px sans-serif";
+        const saisyoTextWidth = this.#textWidth(context, saisyoText);
+        context.fillText(saisyoText, (canvas.width - saisyoTextWidth) / 2, 300);
+
+        const tudukiText = "続きから";
+        context.font = "32px sans-serif";
+        const tudukiTextWidth = this.#textWidth(context, tudukiText);
+        context.fillText(tudukiText, (canvas.width - tudukiTextWidth) / 2, 360);
+    }
+
+    #textWidth(context, text) {
+        const measure = context.measureText(text);
+        return measure.width;
     }
 
     onEnd() {
