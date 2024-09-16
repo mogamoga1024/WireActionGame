96a40e690c5438e8d858185cc485c007cd999363
mogamoga <mogamoga.g1thu6@gmail.com>
2024-09-16 13:32:25 +0900

    たったるらりらった～か～なえるよ～

--- scene/tutorial_scene.js
+++ scene/tutorial_scene.js
@@ -2,35 +2,48 @@
 class TutorialScene extends Scene {
     #controlsDescriptionDom = null;
 
-    #textIndex = 0;
-    #textList = [
-        "～地上のとき～",
-        "←→で移動！",
-        "Xでジャンプ！",
-        "Zでワイヤー！\n" +
-        "引っかかったワイヤーはZで戻せる！",
-        "ワイヤーは↑↓←→で方向が指定できる！",
-        "～ぶらさがりのとき～",
-        "Xでジャンプ！\n" +
-        "ワイヤーは解除されるから注意！",
-        "↑でもジャンプ！\n" +
-        "ワイヤーは解除されないから安心！\n" +
-        "ワイヤーを縮めたいときに使おう！",
-        "Zでワイヤー解除！",
-        "タイミングよく←→を押すと揺らせる！",
-        "↓でワイヤーを伸ばせる！",
-        "～その他～",
-        "Cでマップ確認モードになるぞ！\n" +
-        "↑↓←→でマップを確認しよう！",
-        "～最後に～",
-        "いろいろ動かして慣れてくれ！\n" +
-        "画面外の下にも操作方法書いてあるよ！",
-        "終わり！閉廷！以上！皆解散！"
-    ];
+    #textDataIndex = 0;
+    #textDataList = [];
 
     onStart() {
         this.#controlsDescriptionDom = document.querySelector("#controls-description");
         this.#controlsDescriptionDom.innerText = "←:前へ →:次へ Z:説明終了";
+
+        const tmpTextList = [
+            "～地上のとき～",
+            "←→で移動！",
+            "Xでジャンプ！",
+            "Zでワイヤー！\n" +
+            "引っかかったワイヤーはZで戻せる！",
+            "ワイヤーは↑↓←→で方向が指定できる！",
+            "～ぶらさがりのとき～",
+            "Xでジャンプ！\n" +
+            "ワイヤーは解除されるから注意！",
+            "↑でもジャンプ！\n" +
+            "ワイヤーは解除されないから安心！\n" +
+            "ワイヤーを縮めたいときに使おう！",
+            "Zでワイヤー解除！",
+            "タイミングよく←→を押すと揺らせる！",
+            "↓でワイヤーを伸ばせる！",
+            "～その他～",
+            "Cでマップ確認モードになるぞ！\n" +
+            "↑↓←→でマップを確認しよう！",
+            "～最後に～",
+            "いろいろ動かして慣れてくれ！\n" +
+            "画面外の下にも操作方法書いてあるよ！",
+            "終わり！閉廷！以上！皆解散！"
+        ];
+
+        let section = "";
+        for (const text of tmpTextList) {
+            if (text[0] === "～") {
+                section = text;
+                this.#textDataList.push({section: "", text});
+            }
+            else {
+                this.#textDataList.push({section, text});
+            }
+        }
 
         this.#draw();
     }
@@ -40,15 +53,18 @@ class TutorialScene extends Scene {
         context.fillRect(0, 0, canvas.width, canvas.height);
 
         context.textBaseline = "top";
-        context.font = "900 40px sans-serif";
         context.fillStyle = "#FFFFFF";
 
-        const text = this.#textList[this.#textIndex];
+        const {section, text} = this.#textDataList[this.#textDataIndex];
         const lineTextList = text.split("\n");
         const textSizeList = lineTextList.map(lineText => measureText(context, lineText))
         const totalTextHeight = textSizeList.reduce((acc, cur) => acc + cur.height, 0);
         let y = (canvas.height - totalTextHeight)/2;
         for (let i = 0; i < lineTextList.length; i++) {
+            context.font = "900 28px sans-serif";
+            context.fillText(section, 40, 40);
+
+            context.font = "900 40px sans-serif";
             const lineText = lineTextList[i];
             const {width: textWidth, height: textHeight} = textSizeList[i];
             context.fillText(lineText, (canvas.width - textWidth)/2, y);
@@ -69,27 +85,27 @@ class TutorialScene extends Scene {
         switch (e.key) {
             case "ArrowLeft": {
                 e.preventDefault();
-                if (this.#textIndex > 0) {
+                if (this.#textDataIndex > 0) {
                     SoundStorage.get("ドンッ").play();
-                    this.#textIndex--;
+                    this.#textDataIndex--;
+                    this.#draw();
+                }
+                return;
+            }
+            case "ArrowRight": case "x": {
+                e.preventDefault();
+                if (this.#textDataIndex === this.#textDataList.length - 1) {
+                    SceneManager.finish();
+                }
+                else {
+                    SoundStorage.get("ドンッ").play();
+                    this.#textDataIndex++;
                     this.#draw();
                 }
                 return;
             }
             case "z": {
                 SceneManager.finish();
-                return;
-            }
-            default: {
-                e.preventDefault();
-                if (this.#textIndex === this.#textList.length - 1) {
-                    SceneManager.finish();
-                }
-                else {
-                    SoundStorage.get("ドンッ").play();
-                    this.#textIndex++;
-                    this.#draw();
-                }
                 return;
             }
         }
