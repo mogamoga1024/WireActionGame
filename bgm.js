5620153dd85986d998d1f53a5ed55f5b2d0f69be
mogamoga <mogamoga.g1thu6@gmail.com>
2024-09-20 15:26:55 +0900

    ええやん

--- bgm.js
+++ bgm.js
@@ -16,6 +16,8 @@ class BGM {
     static async start() {
         await Tone.start();
         this.#isPlaying = true;
+
+        await this.#loadPiano();
         this.#playMelody1();
     }
 
@@ -24,10 +26,48 @@ class BGM {
         this.#stopMelody();
     }
 
-    static #initSynths() {
-        const options = {volume: -5};
-        this.#synth1 = new Tone.Synth(options).toDestination();
-        this.#synth2 = new Tone.Synth(options).toDestination();
+    static #loadPiano() {
+        let timer = 0;
+        return new Promise(resolve => {
+            this.#synth1 = null;
+            this.#synth2 = null;
+            timer = setTimeout(() => {
+                const options = {volume: -5};
+                this.#synth1 = new Tone.Synth(options).toDestination();
+                this.#synth2 = new Tone.Synth(options).toDestination();
+                resolve();
+            }, 60 * 1000);
+            const urls = {
+                C2: "C2.mp3", A2: "A2.mp3", C3: "C3.mp3", A3: "A3.mp3",
+                C4: "C4.mp3", A4: "A4.mp3", C5: "C5.mp3", A5: "A5.mp3",
+                C6: "C6.mp3", A6: "A6.mp3",
+            };
+            const baseUrl = "https://tonejs.github.io/audio/salamander/";
+            const envelope = {
+                attack: 0,
+                release: 1
+            };
+            const piano1 = new Tone.Sampler({
+                urls, baseUrl, envelope,
+                onload: () => {
+                    this.#synth1 = piano1;
+                    if (this.#synth2 !== null) {
+                        clearTimeout(timer);
+                        resolve();
+                    }
+                }
+            }).toDestination();
+            const piano2 = new Tone.Sampler({
+                urls, baseUrl, envelope,
+                onload: () => {
+                    this.#synth2 = piano2;
+                    if (this.#synth1 !== null) {
+                        clearTimeout(timer);
+                        resolve();
+                    }
+                }
+            }).toDestination();
+        });
     }
 
     static #stopMelody() {
@@ -42,20 +82,17 @@ class BGM {
     }
 
     static #playMelody1() {
-        this.#stopMelody();
-        this.#initSynths();
-    
         let time = Tone.now();
         const startTime = time;
     
         this.#synth1.triggerAttackRelease("C5", "4n", time); time += t4n;
-        this.#synth1.triggerAttackRelease("G5", "8n", time); time += t4n;
+        this.#synth1.triggerAttackRelease("G5", "4n", time); time += t4n;
         this.#synth1.triggerAttackRelease("B4", "4n", time); time += t4n;
-        this.#synth1.triggerAttackRelease("G5", "8n", time); time += t4n;
+        this.#synth1.triggerAttackRelease("G5", "4n", time); time += t4n;
         this.#synth1.triggerAttackRelease("A4", "4n", time); time += t4n;
-        this.#synth1.triggerAttackRelease("F5", "8n", time); time += t4n;
+        this.#synth1.triggerAttackRelease("F5", "4n", time); time += t4n;
         this.#synth1.triggerAttackRelease("B4", "4n", time); time += t4n;
-        this.#synth1.triggerAttackRelease("G5", "8n", time); time += t4n;
+        this.#synth1.triggerAttackRelease("G5", "4n", time); time += t4n;
     
         this.#timer1 = setTimeout(() => {
             this.#playMelody2();
@@ -63,9 +100,6 @@ class BGM {
     }
 
     static #playMelody2() {
-        this.#stopMelody();
-        this.#initSynths();
-        
         let time = Tone.now();
         const startTime = time;
         
