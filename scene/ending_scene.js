8ab8b0c60098b3d629f9bb302b734cdaf670f8b5
mogamoga <mogamoga.g1thu6@gmail.com>
2024-09-25 18:27:02 +0900

    エンディング 読ませたい

--- scene/ending_scene.js
+++ scene/ending_scene.js
@@ -4,22 +4,26 @@ class EndingScene extends Scene {
     #komaList = [
         {
             backgroundImage: null,
+            sound: null,
             text: "ついに光の場所にたどり着いた植木鉢くん。\n" +
                   "その瞬間、彼はすべてを思い出しました。"
         },
         {
             backgroundImage: null,
+            sound: null,
             text: "自分を大切に育ててくれたおじさんのこと。\n" +
                   "そして、真夏の日におじさんを守るために\n" +
                   "バレーボールを受けて割れてしまったこと。"
         },
         {
             backgroundImage: null,
+            sound: null,
             text: "何より今まで、おじさんに「ありがとう」を伝えられなかったことが、\n" +
                   "彼をずっと縛り続けていたのです。"
         },
         {
             backgroundImage: null,
+            sound: null,
             text: "そのとき、光の中からGOという神様が現れました。\n" +
                   "「植木鉢くん、長い間、苦しんだね。\n" +
                   "未練を抱えて迷い続ける道は、つらかっただろう。\n" +
@@ -27,26 +31,31 @@ class EndingScene extends Scene {
         },
         {
             backgroundImage: null,
+            sound: null,
             text: "GOは優しく微笑みました。\n" +
                   "「さあ、何でも叶えてあげよう。」"
         },
         {
             backgroundImage: null,
+            sound: null,
             text: "植木鉢くんは、少しの間、考えてから言いました。\n" +
                   "「おじさんに、ありがとうを伝えてほしいんだ。\n" +
                   "それができれば、僕はもう十分だよ。」"
         },
         {
             backgroundImage: null,
+            sound: null,
             text: "GOが静かにうなずくと、光がますます暖かく、まばゆく輝きました。\n" +
                   "植木鉢くんは安らかな微笑みを浮かべながら、\n" +
                   "静かに光の中へ消えていきました。"
         },
         {
             backgroundImage: null,
+            sound: null,
             text: "おしまい"
         },
     ];
+    #sound = null;
     #canKeyDown = true;
 
     onStart() {
