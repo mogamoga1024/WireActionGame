1ce7001cbfcc278c3fd2f881026171eb231592e7
mogamoga <mogamoga.g1thu6@gmail.com>
2024-09-27 02:29:49 +0900

    sound 移動

---
+++ asset_utils.js
@@ -0,0 +1,27 @@
+
+function loadImage(path) {
+    const image = new Image();
+    image.src = path;
+    return new Promise(resolve => {
+        image.onload = () => {
+            resolve(image);
+        };
+        image.onerror = () => {
+            resolve(image);
+        };
+    });
+}
+
+function loadSound(key) {
+    let {path, volume = 1} = soundParams(key);
+    volume *= 0.75;
+    const audio = new Audio(path);
+    return new Promise(resolve => {
+        audio.oncanplaythrough = () => {
+            resolve(new Sound(audio, volume));
+        };
+        audio.onerror = () => {
+            resolve(new Sound(audio, volume));
+        };
+    });
+}
--- common_utils.js
+++ common_utils.js
@@ -3,33 +3,6 @@ function rad2deg(radian) {
     return radian * (180 / Math.PI);
 }
 
-function loadImage(path) {
-    const image = new Image();
-    image.src = path;
-    return new Promise(resolve => {
-        image.onload = () => {
-            resolve(image);
-        };
-        image.onerror = () => {
-            resolve(image);
-        };
-    });
-}
-
-function loadSound(key) {
-    let {path, volume = 1} = soundParams(key);
-    volume *= 0.75;
-    const audio = new Audio(path);
-    return new Promise(resolve => {
-        audio.oncanplaythrough = () => {
-            resolve(new Sound(audio, volume));
-        };
-        audio.onerror = () => {
-            resolve(new Sound(audio, volume));
-        };
-    });
-}
-
 function loadTestWait() {
     return new Promise(resolve => {
         setTimeout(resolve, 3000);
